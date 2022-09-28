6e9bcd758091e6b604568e525583e86159f0540e
Natalie <natalieliles@gmail.com>
2022-09-27 20:23:03 -0400

    update

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ var bodyParser = require("body-parser");
 
 //Connecting the Database
 connectDB();
-
 const normalizePort = val => {
   const port = parseInt(val, 10);
 
@@ -28,36 +27,14 @@ const normalizePort = val => {
 const port = normalizePort(process.env.PORT || '5000');
 app.set('port', port);
 
-const errorHandler = error => {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-  const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges.');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use.');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-};
-
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
 
-server.on('listening', () => {
-  const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-  console.log('Listening on ' + bind);
+const server = app.listen(port, function () {
+  console.log("Server started successfully");
 });
 
 // Handling Error
