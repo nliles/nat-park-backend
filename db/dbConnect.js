74bca74be6195fd2a1d71f276e6268d79b935df3
Natalie <natalie.liles@mavenclinic.com>
2023-01-20 12:02:15 -0500

    fix error

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 require('dotenv').config()
 
   async function dbConnect() {
+    mongoose.set('strictQuery', false);
     mongoose
       .connect(
           process.env.DB_URL
