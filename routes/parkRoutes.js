86820140522f8bfbf19567c786f73ffb1414c56d
Natalie <natalie.liles@mavenclinic.com>
2023-01-22 19:58:29 -0500

    add create route

--- routes/parkRoutes.js
+++ routes/parkRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { getParks, updateParks } = require("../park");
+const { getParks, createParks, updateParks } = require("../park");
 const isAuthenticated = require("../middleware/isAuthenticated");
 
 router.route("/park").get(isAuthenticated, getParks);
-router.route("/park").post(isAuthenticated, updateParks);
+router.route("/park").post(isAuthenticated, createParks);
+router.route("/park").patch(isAuthenticated, updateParks);
 
 module.exports = router;
