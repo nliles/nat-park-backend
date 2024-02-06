94c58d60a4b60ae4a06706bba684a51757dad02b
Natalie Liles <natalie.liles@mavenclinic.com>
2024-02-05 21:55:19 -0500

    clean up db connect

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -3,15 +3,13 @@ require("dotenv").config();
 
 async function dbConnect() {
   mongoose.set("strictQuery", false);
-  mongoose
-    .connect(process.env.DB_URL)
-    .then(() => {
-      console.log("Successfully connected to MongoDB Atlas!");
-    })
-    .catch((error) => {
+  mongodb.connect(
+    process.env.DB_URL,
+    (err) => {
       console.log("Unable to connect to MongoDB Atlas!");
-      console.error(error);
-    });
+      console.error(err);
+    }
+  );
 }
 
 module.exports = dbConnect;
