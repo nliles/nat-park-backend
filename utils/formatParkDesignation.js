5d652c495357d3272e7ea4027933abf8286f11c8
Natalie Liles <natalie.liles@mavenclinic.com>
2024-03-27 18:37:34 -0400

    apply prettier

--- utils/formatParkDesignation.js
+++ utils/formatParkDesignation.js
@@ -1,21 +1,22 @@
 const kebabCase = require("lodash.kebabcase");
 const camelCase = require("lodash.camelcase");
+const startCase = require("lodash.startcase");
 const { PARK_INFO } = require("../constants/parkData");
 
 const formatParkDesignation = (park) => {
   const designations = Object.keys(PARK_INFO);
   const formattedDesignation = camelCase(park.designation);
   if (designations.includes(formattedDesignation)) {
-    return park.designation;
+    park.designation;
   }
   const found = designations.find((parkKey) => {
     if (PARK_INFO[parkKey].codes.includes(park.parkCode)) {
-      return parkKey;
+      return startCase(parkKey);
     } else {
       return undefined;
     }
   });
-  return found || "otherDesignation";
+  return found || "Other Designation";
 };
 
 module.exports = {
