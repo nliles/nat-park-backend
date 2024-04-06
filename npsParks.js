fa7bd5ed2196db3e92943d743a2976b7833c9a56
Natalie Liles <natalie.liles@mavenclinic.com>
2024-04-05 21:51:13 -0400

    update data formatting

--- npsParks.js
+++ npsParks.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const camelCase = require("lodash.camelcase");
 const { NPS_URL } = require("./constants/api");
 const { ALL_CODES } = require("./constants/parkData");
 const { formatParks } = require("./utils/formatParks");
@@ -8,6 +9,15 @@ exports.getNPSParks = async (req, res, next) => {
   const API_KEY = process.env.NPS_API_KEY;
   const URL = `${NPS_URL}?parkCode=${ALL_CODES}}&limit=496&sort=fullName&api_key=${API_KEY}`;
   const response = await axios.get(URL);
-  const data = sortParks(formatParks(response.data.data));
-  res.json(data);
+  const parks = sortParks(formatParks(response.data.data));
+  const formattedParks = parks.reduce((acc, curr) => {
+    const formattedDesignation = camelCase(curr.designation)
+    if (acc[formattedDesignation]) {
+      acc[formattedDesignation].push(curr)
+    } else {
+      acc[formattedDesignation] = [curr]
+    }
+    return acc;
+  }, {})
+  res.json({ parks: formattedParks, total: parks.length });
 };
