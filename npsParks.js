c0574d9a49c589553295303bfa09c84008112d00
Natalie Liles <natalie.liles@mavenclinic.com>
2024-04-05 22:00:19 -0400

    update helper function

--- npsParks.js
+++ npsParks.js
@@ -4,20 +4,13 @@ const { NPS_URL } = require("./constants/api");
 const { ALL_CODES } = require("./constants/parkData");
 const { formatParks } = require("./utils/formatParks");
 const { sortParks } = require("./utils/sortParks");
+const { formatParkData } = require("./utils/formatParkData");
 
 exports.getNPSParks = async (req, res, next) => {
   const API_KEY = process.env.NPS_API_KEY;
   const URL = `${NPS_URL}?parkCode=${ALL_CODES}}&limit=496&sort=fullName&api_key=${API_KEY}`;
   const response = await axios.get(URL);
   const parks = sortParks(formatParks(response.data.data));
-  const formattedParks = parks.reduce((acc, curr) => {
-    const formattedDesignation = camelCase(curr.designation)
-    if (acc[formattedDesignation]) {
-      acc[formattedDesignation].push(curr)
-    } else {
-      acc[formattedDesignation] = [curr]
-    }
-    return acc;
-  }, {})
+  const formattedParks = formatParkData(parks);
   res.json({ parks: formattedParks, total: parks.length });
 };
---
+++ utils/formatParkData.js
@@ -0,0 +1,15 @@
+const camelCase = require("lodash.camelcase");
+
+const formattedParks = (parks) => parks.reduce((acc, curr) => {
+    const formattedDesignation = camelCase(curr.designation)
+    if (acc[formattedDesignation]) {
+      acc[formattedDesignation].push(curr)
+    } else {
+      acc[formattedDesignation] = [curr]
+    }
+    return acc;
+  }, {})
+
+  module.exports = {
+    formattedParks,
+  };
